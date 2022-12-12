e729d98715bfad45073a9a8faaac126adcf3f786
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-12-12 16:54:50 -0500

    report next

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,25 +1,15 @@
 package io.zipcoder;
-
-import io.zipcoder.utils.FileReader;
-import io.zipcoder.utils.Item;
-import io.zipcoder.utils.match.Match;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class GroceryReporter {
-
-    private String fileName = "RawInput.txt";
+    @Override
+    public String toString() {
+        return "GroceryReporter{}";
+    }
 
     public GroceryReporter(String fileName) {
         this.fileName = fileName;
     }
 
-    public GroceryReporter() {
-    }
-
     public static void main(String[] args) {
+        GroceryReporter groceryReporter = new GroceryReporter("RawInput.txt");
      }
 }
