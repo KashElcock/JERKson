c878f3a20a0ea1e0d644b8d9b14a1e5e32ead67d
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-12-12 16:48:43 -0500

    23/24 passed

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -10,65 +10,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GroceryReporter {
+
+    private String fileName = "RawInput.txt";
+
+    public GroceryReporter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public GroceryReporter() {
+    }
+
     public static void main(String[] args) {
-
-        String[] jerkItems = new GroceryReporter("RawInput.txt").splitFileIntoJerkItems();
-        ArrayList<Item> mildItems = new ArrayList<>();
-        for (String input : jerkItems) {
-            Pattern p = Pattern.compile("(\\w+):([^^;*@!:%]+)");
-            Matcher m = p.matcher(input);
-            String nameValue = "";
-            String priceValue = "";
-            String typeValue = "";
-            String expirationValue = "";
-            while (m.find()) {
-                switch (m.group(1).toLowerCase()) {
-                    case "name":
-                        String[] words = m.group(2).split(" ");
-                        StringBuilder sb = new StringBuilder();
-                        for (String word : words) {
-                            sb.append(word.substring(0, 1).toUpperCase());
-                            sb.append(word.substring(1).toLowerCase());
-                        } nameValue = sb.toString();
-                        break;
-                    case "price":
-                        priceValue = m.group(2);
-                        break;
-                    case "type":
-                        typeValue = m.group(2);
-                        break;
-                    case "expiration":
-                        expirationValue = m.group(2);
-                        break;
-                }
-            } try{
-                Item item = new Item(nameValue, Double.parseDouble(priceValue), typeValue, expirationValue);
-                System.out.println(item);
-                mildItems.add(item);
-            } catch (NumberFormatException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-
-
-        }
-    }
-
-    public String getCleanReportText() {
-        return null;
-    }
-    //method to split the file into an array of strings representing each jerk item
-    public String[] splitFileIntoJerkItems() {
-        ArrayList<String> objects = new ArrayList<>(List.of(new GroceryReporter("RawInput.txt").getOriginalFileText().split("##")));
-        System.out.println(objects);
-        return objects.toArray(new String[0]);
-    }
-    private final String originalFileText;
-    public GroceryReporter(String jerksonFileName) {
-        this.originalFileText = FileReader.readFile(jerksonFileName);
-    }
-
-    public String getOriginalFileText() {
-        return originalFileText;
-    }
-
+     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,91 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> parseItemList = new ArrayList<>();
+        String[] splitString = valueToParse.split("##");
+        try {
+            for (String input : splitString) {
+                Pattern p = Pattern.compile("(\\w+)+[;%:*^@!]([^^;*@!:%#]+)");
+                Matcher m = p.matcher(input);
+                String nameValue = "";
+                String priceValue = "";
+                String typeValue = "";
+                String expirationValue = "";
+                while (m.find()) {
+                    switch (m.group(1).toLowerCase()) {
+                        case "name" -> {
+                            nameValue = m.group(2);
+                            nameValue = nameValue.toLowerCase();
+                        }
+                        case "price" -> {
+                            priceValue = m.group(2);
+                            priceValue = priceValue.toLowerCase();
+                        }
+                        case "type" -> {
+                            typeValue = m.group(2);
+                            typeValue = typeValue.toLowerCase();
+                        }
+                        case "expiration" -> expirationValue = m.group(2);
+                    }
+                }
+                try {
+                    Item item = new Item(nameValue, Double.parseDouble(priceValue), typeValue, expirationValue);
+                    System.out.println(item);
+                    if(nameValue.equals("") || priceValue.equals("") || typeValue.equals("") || expirationValue.equals("")){
+                        throw new ItemParseException();
+                    }
+                    parseItemList.add(item);
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return parseItemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        Pattern p = Pattern.compile("(\\w+)+[;:%*^@!]([^^;*@!:%#]+)");
+        Matcher m = p.matcher(singleItem);
+        String nameValue = "";
+        String priceValue = "";
+        String typeValue = "";
+        String expirationValue = "";
+        Item item = null;
+        try {
+        while (m.find()) {
+            switch (m.group(1).toLowerCase()) {
+                case "name" -> {
+                    nameValue = m.group(2);
+                    nameValue = nameValue.toLowerCase();
+                }
+                case "price" -> {
+                    priceValue = m.group(2);
+                    priceValue = priceValue.toLowerCase();
+                }
+                case "type" -> {
+                    typeValue = m.group(2);
+                    typeValue = typeValue.toLowerCase();
+                }
+                case "expiration" -> expirationValue = m.group(2);
+            }
+        }
+            item = new Item(nameValue, Double.parseDouble(priceValue), typeValue, expirationValue);
+            System.out.println(item);
+        } catch (NumberFormatException e) {
+            throw new ItemParseException();
+        }
+        if(nameValue.equals("") || priceValue.equals("") || typeValue.equals("") || expirationValue.equals("")){
+            throw new ItemParseException();
+        }
+            return item;
     }
 }
